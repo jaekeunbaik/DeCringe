867322c6fb23414f9e628ed8facf0f9c04da5731
jaekeunbaik <jaekeunbaik@gmail.com>
2026-08-08 00:07:46 +0900

    seo: add direct meta tag inside head element

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta name="google-site-verification" content="coYlpU9HVJXVvz9RmjkNZQHXaurqiUiewwUGPb8qXCw" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <script
